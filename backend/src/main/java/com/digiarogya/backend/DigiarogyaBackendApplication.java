b206f9280d0037ab7a29d598bd2f9efeb402f38d
Preet2099x <preet2099x@gmail.com>
2025-12-15 01:34:06 +0530

    intialized backend

---
+++ backend/src/main/java/com/digiarogya/backend/DigiarogyaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.digiarogya.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigiarogyaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigiarogyaBackendApplication.class, args);
+	}
+
+}
