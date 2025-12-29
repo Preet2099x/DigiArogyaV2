a908491cba4fdd013fb4a40f199046e96b160825
Preet2099x <preet2099x@gmail.com>
2025-12-29 20:46:28 +0530

    Added updated RecordTypes to PatientRecords

--- backend/src/main/java/com/digiarogya/backend/entity/RecordType.java
+++ backend/src/main/java/com/digiarogya/backend/entity/RecordType.java
@@ -1,10 +1,11 @@
 package com.digiarogya.backend.entity;
 
 public enum RecordType {
-    LAB_REPORT,
-    PRESCRIPTION,
-    DIAGNOSIS,
-    DISCHARGE_SUMMARY,
     NOTE,
-    OTHER
+    DIAGNOSIS,
+    PRESCRIPTION,
+    LAB_RESULT,
+    IMAGING,
+    VITALS,
+    PROCEDURE
 }
